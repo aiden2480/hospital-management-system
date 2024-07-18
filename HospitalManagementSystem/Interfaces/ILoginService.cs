b37f4e6cbde62fa5abbda82e26cbb037412aef4c
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-07-18 15:47:34 +1000

    Add spectre

--- HospitalManagementSystem/Interfaces/ILoginService.cs
+++ HospitalManagementSystem/Interfaces/ILoginService.cs
@@ -4,5 +4,11 @@ namespace HospitalManagementSystem.Interfaces;
 
 public interface ILoginService
 {
+    /// <summary>
+    /// Attempts to log the user into the application with the given user ID and password
+    /// </summary>
+    /// <param name="userId">User ID. Can be doctor, patient, admin</param>
+    /// <param name="password">The password for the account</param>
+    /// <returns>The requested user, if the credentials were correct, otherwise null</returns>
     public LoginCapableUser? AttemptLogin(int userId, string password);
 }
