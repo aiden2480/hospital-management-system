bf296d7acd734b83df9f8ce127e079d7e60ad2ad
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-07-19 18:58:09 +1000

    Extension methods for ToTable

---
+++ HospitalManagementSystem/Extension/PluralExtensions.cs
@@ -0,0 +1,40 @@
+﻿using HospitalManagementSystem.Entity;
+using Spectre.Console;
+
+namespace HospitalManagementSystem.Extension;
+
+static internal class PluralExtensions
+{
+    public static Table ToTable(this IEnumerable<LoginCapableUserWithDetails> users)
+    {
+        var table = new Table()
+            .AddColumn("Full Name")
+            .AddColumn("Email")
+            .AddColumn("Phone")
+            .AddColumn("Address");
+
+        foreach (var user in users)
+        {
+            table.AddRow(user.FullName, user.Email, user.PhoneNumber, user.FullAddr);
+        }
+
+        return table;
+    }
+
+    public static Table ToTable(this IEnumerable<Appointment> appointments)
+    {
+        var table = new Table()
+            .AddColumn("Appointment ID")
+            .AddColumn("Doctor")
+            .AddColumn("Patient")
+            .AddColumn("Time")
+            .AddColumn("Description");
+
+        foreach (var a in appointments)
+        {
+            table.AddRow(a.Id.ToString(), a.Doctor.FullName, a.Patient.FullName, a.ScheduledTime.ToString(), a.Description);
+        }
+
+        return table;
+    }
+}
---
+++ HospitalManagementSystem/Extension/SingularExtensions.cs
@@ -0,0 +1,23 @@
+﻿using HospitalManagementSystem.Entity;
+using Spectre.Console;
+
+namespace HospitalManagementSystem.Extension
+{
+    static internal class SingularExtensions
+    {
+        public static Table ToTable(this LoginCapableUserWithDetails user)
+        {
+            var type = user.GetType().Name;
+            var table = new Table()
+                .AddColumns("", "")
+                .AddRow($"{type} ID", user.Id.ToString())
+                .AddRow("Full name", user.FullName)
+                .AddRow("Address", user.FullAddr)
+                .AddRow("Email", user.Email)
+                .AddRow("Phone", user.PhoneNumber)
+                .HideHeaders();
+
+            return table;
+        }
+    }
+}
