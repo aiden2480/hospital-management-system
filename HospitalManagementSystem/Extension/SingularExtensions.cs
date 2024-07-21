2e0d275cd6bc486e7b9e290224c4be63817d1429
Aiden Gardner <19619206+aiden2480@users.noreply.github.com>
2024-07-21 18:32:06 +1000

    Appointment.ToTable()

--- HospitalManagementSystem/Extension/SingularExtensions.cs
+++ HospitalManagementSystem/Extension/SingularExtensions.cs
@@ -6,18 +6,23 @@ namespace HospitalManagementSystem.Extension
     static internal class SingularExtensions
     {
         public static Table ToTable(this AbstractUserWithAppointments user)
-        {
-            var type = user.GetType().Name;
-            var table = new Table()
+            => new Table()
                 .AddColumns("", "")
-                .AddRow($"{type} ID", user.Id.ToString())
+                .AddRow($"{user.GetType().Name} ID", user.Id.ToString())
                 .AddRow("Full name", user.FullName)
                 .AddRow("Address", user.FullAddr)
                 .AddRow("Email", user.Email)
                 .AddRow("Phone", user.PhoneNumber)
                 .HideHeaders();
 
-            return table;
-        }
+        public static Table ToTable(this Appointment appointment)
+            => new Table()
+                .AddColumns("", "")
+                .AddRow("Appointment ID", appointment.Id.ToString())
+                .AddRow("Doctor", appointment.Doctor.FullName)
+                .AddRow("Patient", appointment.Patient.FullName)
+                .AddRow("Time", appointment.ScheduledTime.ToString())
+                .AddRow("Description", appointment.Description)
+                .HideHeaders();
     }
 }
